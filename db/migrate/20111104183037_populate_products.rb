c25b5d359604ccfc393a436fff7466dbae622d5b
lucasmpavelski <lmpavelski@yahoo.com.br>
2011-11-06 15:51:59 -0200

    fixed prices comma

--- db/migrate/20111104183037_populate_products.rb
+++ db/migrate/20111104183037_populate_products.rb
@@ -5,7 +5,7 @@ class PopulateProducts < ActiveRecord::Migration
       p.name = "Venlift OD"
       p.code = 00001
       p.provider = Provider.where(:name => "Sapucaí")[0]
-      p.price = "77,17"
+      p.price = "77.17"
       p.amount = 40
       p.laboratory = "Ativus"
     end
@@ -14,7 +14,7 @@ class PopulateProducts < ActiveRecord::Migration
       p.name = "Doril"
       p.code = 00002
       p.provider = Provider.where(:name => "Farmacêutica")[0]
-      p.price = "4,50"
+      p.price = "4.50"
       p.amount = 200
       p.laboratory = "Medley"
     end
@@ -23,7 +23,7 @@ class PopulateProducts < ActiveRecord::Migration
       p.name = "Anador"
       p.code = 00003
       p.provider = Provider.where(:name => "DISDROL")[0]
-      p.price = "11,29"
+      p.price = "11.29"
       p.amount = 100
       p.laboratory = "HALEX ISTAR"
     end
@@ -32,7 +32,7 @@ class PopulateProducts < ActiveRecord::Migration
       p.name = "Dipirona"
       p.code = 00004
       p.provider = Provider.where(:name => "Droga Rápida")[0]
-      p.price = "9,79"
+      p.price = "9.79"
       p.amount = 50
       p.laboratory = "HYPERMARCAS"
     end
@@ -41,7 +41,7 @@ class PopulateProducts < ActiveRecord::Migration
       p.name = "Torsilax"
       p.code = 00005
       p.provider = Provider.where(:name => "Suporte Medicamentos")[0]
-      p.price = "9,21"
+      p.price = "9.21"
       p.amount = 150
       p.laboratory = "LAPA"
     end
@@ -50,7 +50,7 @@ class PopulateProducts < ActiveRecord::Migration
       p.name = "Trifen"
       p.code = 00006
       p.provider = Provider.where(:name => "FARMACLINIC")[0]
-      p.price = "5,43"
+      p.price = "5.43"
       p.amount = 50
       p.laboratory = "PFIZER"
     end
@@ -59,7 +59,7 @@ class PopulateProducts < ActiveRecord::Migration
       p.name = "Paracetamol"
       p.code = 00007
       p.provider = Provider.where(:name => "LIMED MOGI")[0]
-      p.price = "3,50"
+      p.price = "3.50"
       p.amount = 200
       p.laboratory = "Pharmacia & Upjohn Ltda."
     end
@@ -68,7 +68,7 @@ class PopulateProducts < ActiveRecord::Migration
       p.name = "Amoxilina"
       p.code = 8
       p.provider = Provider.where(:name => "DMED")[0]
-      p.price = "18,38"
+      p.price = "18.38"
       p.amount = 150
       p.laboratory = "DUCTO"
     end
@@ -78,7 +78,7 @@ class PopulateProducts < ActiveRecord::Migration
       p.name = "Pantoprazol 40mg"
       p.code = 9
       p.provider = Provider.where(:name => "Suporte Medicamentos")[0]
-      p.price = "43,08"
+      p.price = "43.08"
       p.amount = 30
       p.laboratory = "NOVARTIS"
     end
@@ -87,7 +87,7 @@ class PopulateProducts < ActiveRecord::Migration
       p.name = "Preserv Lite"
       p.code = 10
       p.provider = Provider.where(:name => "Sapucaí")[0]
-      p.price = "3,50"
+      p.price = "3.50"
       p.amount = 30
       p.laboratory = "BLAUSIEGEL"
     end
@@ -96,7 +96,7 @@ class PopulateProducts < ActiveRecord::Migration
       p.name = "Diclofenaco Potássico"
       p.code = 00011
       p.provider = Provider.where(:name => "DMED")[0]
-      p.price = "6,75"
+      p.price = "6.75"
       p.amount = 100
       p.laboratory = "NEO QUIMICA"
     end
@@ -105,7 +105,7 @@ class PopulateProducts < ActiveRecord::Migration
       p.name = "Diclofenaco Sódico"
       p.code = 00012
       p.provider = Provider.where(:name => "FARMACLINIC")[0]
-      p.price = "9,45"
+      p.price = "9.45"
       p.amount = 100
       p.laboratory = "ARISTON"
     end
@@ -114,7 +114,7 @@ class PopulateProducts < ActiveRecord::Migration
       p.name = "Meritor"
       p.code = 00013
       p.provider = Provider.where(:name => "Droga Rápida")[0]
-      p.price = "13,87"
+      p.price = "13.87"
       p.amount = 50
       p.laboratory = "ACHÈ"
     end
@@ -123,7 +123,7 @@ class PopulateProducts < ActiveRecord::Migration
       p.name = "Peridona"
       p.code = 00014
       p.provider = Provider.where(:name => "DISDROL")[0]
-      p.price = "9,29"
+      p.price = "9.29"
       p.amount = 100
       p.laboratory = "UCI FARMA"
     end
@@ -132,7 +132,7 @@ class PopulateProducts < ActiveRecord::Migration
       p.name = "Vodol"
       p.code = 00015
       p.provider = Provider.where(:name => "Farmashoprio")[0]
-      p.price = "19,13"
+      p.price = "19.13"
       p.amount = 100
       p.laboratory = "UNIAO QUIMICA"
     end
@@ -141,7 +141,7 @@ class PopulateProducts < ActiveRecord::Migration
       p.name = "Polaramine Líquido"
       p.code = 00016
       p.provider = Provider.where(:name => "Farmacêutica")[0]
-      p.price = "4,16"
+      p.price = "4.16"
       p.amount = 50
       p.laboratory = "Mantecorp"
     end
@@ -150,7 +150,7 @@ class PopulateProducts < ActiveRecord::Migration
       p.name = "Histamin"
       p.code = 00017
       p.provider = Provider.where(:name => "LIMED MOGI")[0]
-      p.price = "5,41"
+      p.price = "5.41"
       p.amount = 100
       p.laboratory = "NEO QUIMICA"
     end
@@ -159,7 +159,7 @@ class PopulateProducts < ActiveRecord::Migration
       p.name = "Coristina D"
       p.code = 00017
       p.provider = Provider.where(:name => "Droga Rápida")[0]
-      p.price = "7,15"
+      p.price = "7.15"
       p.amount = 100
       p.laboratory = "Mantecorp"
     end
@@ -168,7 +168,7 @@ class PopulateProducts < ActiveRecord::Migration
       p.name = "Coristina R"
       p.code = 18
       p.provider = Provider.where(:name => "DISDROL")[0]
-      p.price = "7,15"
+      p.price = "7.15"
       p.amount = 100
       p.laboratory = "Mantecorp"
     end
@@ -177,7 +177,7 @@ class PopulateProducts < ActiveRecord::Migration
       p.name = "Imosec"
       p.code = 19
       p.provider = Provider.where(:name => "DMED")[0]
-      p.price = "6,82"
+      p.price = "6.82"
       p.amount = 100
       p.laboratory = "JANSSEN- CILAG FARMACÊUTICA LTDA."
     end
@@ -186,7 +186,7 @@ class PopulateProducts < ActiveRecord::Migration
       p.name = "Levoxin"
       p.code = 00020
       p.provider = Provider.where(:name => "DMED")[0]
-      p.price = "12,20"
+      p.price = "12.20"
       p.amount = 50
       p.laboratory = "APSEN"
     end
@@ -195,7 +195,7 @@ class PopulateProducts < ActiveRecord::Migration
       p.name = "Peprazol"
       p.code = 00021
       p.provider = Provider.where(:name => "Farmashoprio")[0]
-      p.price = "15,66"
+      p.price = "15.66"
       p.amount = 50
       p.laboratory = "LIBBS"
     end
@@ -204,7 +204,7 @@ class PopulateProducts < ActiveRecord::Migration
       p.name = "Anandron"
       p.code = 00022
       p.provider = Provider.where(:name => "Suporte Medicamentos")[0]
-      p.price = "11,50"
+      p.price = "11.50"
       p.amount = 70
       p.laboratory = "Sanofi Aventis Farmacêutica Ltda "
     end
@@ -213,7 +213,7 @@ class PopulateProducts < ActiveRecord::Migration
       p.name = "Cefaliv"
       p.code = 23
       p.provider = Provider.where(:name => "Sapucaí")[0]
-      p.price = "10,70"
+      p.price = "10.70"
       p.amount = 60
       p.laboratory = "ACHÈ"
     end
@@ -222,7 +222,7 @@ class PopulateProducts < ActiveRecord::Migration
       p.name = "Cefalium"
       p.code = 24
       p.provider = Provider.where(:name => "Farmacêutica")[0]
-      p.price = "14,10"
+      p.price = "14.10"
       p.amount = 40
       p.laboratory = "ACHÈ"
     end
@@ -231,7 +231,7 @@ class PopulateProducts < ActiveRecord::Migration
       p.name = "Clotrimazol"
       p.code = 25
       p.provider = Provider.where(:name => "DISDROL")[0]
-      p.price = "6,34"
+      p.price = "6.34"
       p.amount = 30
       p.laboratory = "NEO QUIMICA"
     end
