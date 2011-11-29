70760e5044e172a714786007515273fc0479ac95
lucasmpavelski <lmpavelski@yahoo.com.br>
2011-11-29 12:41:00 -0200

    more translates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -60,7 +60,6 @@ class UsersController < ApplicationController
   # PUT /users/1.json
   def update
     @user = User.find(params[:id])
-
     respond_to do |format|
       if @user.update_attributes(params[:user])
         format.html { redirect_to @user, :notice => 'User was successfully updated.' }
